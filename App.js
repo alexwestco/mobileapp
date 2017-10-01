a43be095d1cbd5f471cc40ad9848177a6d6260e5
Nikos Rentas <cse2343@gmail.com>
2017-10-01 23:45:46 +0300

    Second commit: ScrollView and Dumy data

--- App.js
+++ App.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { Image, View, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import { Container, Content, Card, CardItem, Text } from 'native-base';
+import DetailData from './DetailData.js';
+
+const DATA = [
+    { id: 1, text: 'kappa keepo1', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 2, text: 'kappa keepo2', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 3, text: 'kappa keepo3', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 4, text: 'kappa keepo4', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 5, text: 'kappa keepo5', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 6, text: 'kappa keepo6', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 7, text: 'kappa keepo7', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' },
+    { id: 8, text: 'kappa keepo8', uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' }
+
+];
 export default class CardImageExample extends Component {
-    render() {
+    renderCard(item) {
         return (
-            <Container>
-                <Header />
+            <Container
+                key={item.id}
+                style={{
+                    borderRadius: 4,
+                    borderWidth: 0.5,
+                    borderColor: 'red',
+                    height: 300,}}>
                 <Content>
                     <Card>
                         <CardItem>
                             <View style={{ flex: 1, flexDirection: 'column' }}>
-                            <View style={{flexDirection: 'row', justifyContent: 'space-between' }}>                         
-                                <Text>kappa reality</Text>
-                                <Image source={require('./more.png')} />
-                            </View>
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                    <Text>{item.text}</Text>
+                                    <TouchableOpacity onPress={() => { Alert.alert('kappa reality')} }>
+                                        <View>
+                                            <Image source={require('./more.png')} />
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
                                 <View>
-                                    <Text>kappa reality</Text>
+                                    <Text>{item.text}</Text>
                                 </View>
                             </View>
                         </CardItem>
                         <CardItem cardBody>
-                            <Image source={{ uri: 'http://cityvibes.gr/media/Club/DemoClub/profile/asd.jpg' }} style={{ height: 200, width: null, flex: 1 }} />
+                            <Image source={{ uri: item.uri }} style={{ height: 200, width: null, flex: 1 }} />
                         </CardItem>
                     </Card>
                 </Content>
             </Container>
+            );
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView>
+                    <DetailData
+                        data={DATA}
+                        renderCard={this.renderCard}
+                    />
+                </ScrollView>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#000',
+        marginTop: 20,
+    },
+});
